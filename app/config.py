4384b0d591bd28742e3fbefa661272a7e4ed44fd
wiktorjurkowski <w.jurkowski@ardeo.com>
2025-06-04 22:46:34 +0100

    missing config

--- app/config.py
+++ app/config.py
@@ -20,6 +20,7 @@ GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
 MICROSOFT_REDIRECT_URI = os.getenv("MICROSOFT_REDIRECT_URI")
 
 # Microsoft OAuth
-MS_CLIENT_ID = os.getenv("MS_CLIENT_ID")  # <--- This was missing
+MS_CLIENT_ID = os.getenv("MS_CLIENT_ID")
 MS_CLIENT_SECRET = os.getenv("MS_CLIENT_SECRET")
 MS_REDIRECT_URI = os.getenv("MS_REDIRECT_URI")
+MS_TENANT_ID = os.getenv("MS_TENANT_ID")
